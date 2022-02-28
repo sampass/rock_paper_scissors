bed8d058566e32c6d3bd665c7b3fd20024a594d8
Sam Passera <passerasam@gmail.com>
2022-02-28 11:21:40 -0500

    added a single game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock_paper_scissors
+
+this is a five round rock paper scissors game played in the console
--- scripts.js
+++ scripts.js
@@ -6,3 +6,55 @@ function randomNum(min = 1, max = 4) {
     return rand;
 }
 
+function computerPlay(x) {
+    let comp = " ";
+    if (randomNum() == 1) {
+        comp = "rock";
+    }
+    else if (randomNum() == 2) {
+        comp = "scissors";
+    }
+    else {
+        comp = "paper";
+    }
+    return comp;
+}
+
+console.log(computerPlay());
+
+function singleRound(playerSelection , computerSelection) {
+    playerSelection = prompt('Rock, Paper, Scissors?');
+    computerSelection = computerPlay();
+    let result = " ";
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+        result = "You have tied!";
+        return result;
+    }
+    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+        result = "Rock beats scissors. You have won!"
+        return result;
+    }
+    else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
+        result = "Rock beats scissors. You have lost!"
+        return result;
+    }
+    else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+        result = "Paper beats rock. You have won!"
+        return result;
+    }
+    else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+        result = "Paper beats rock. You have lost!"
+        return result;
+    }
+    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+        result = "Scissors beats paper. You have won!"
+        return result;
+    }
+    else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
+        result = "Scissors beats paper. You have lost!"
+        return result;
+    }
+}
+console.log(singleRound());
